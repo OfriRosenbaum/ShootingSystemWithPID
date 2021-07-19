5bbe3e0d67e5e94056a8938c8dfd842c50f7cbdd
OfriRosenbaum <ofriro1000>
2021-07-19 20:51:46 +0300

    Initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public interface CAN {
+
+        int PCM = 0;
+        int SHOOTER_MASTER = 1;
+        int SHOOTER_SLAVE = 2;
+    }
+}
+
+
---
+++ src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -0,0 +1,93 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystemWithPID;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.control.noise.ExponentialFilter;
+import com.spikes2212.control.noise.NoiseReducer;
+import com.spikes2212.dashboard.*;
+import com.spikes2212.util.TalonEncoder;
+import frc.robot.RobotMap;
+
+import java.util.function.Supplier;
+
+public class ShootingSubsystem extends GenericSubsystem {
+
+    public static final double distancePerPulse = 10 / 4096.0;
+    private static RootNamespace shooterNamespace = new RootNamespace("shooter");
+    private static Namespace PID = shooterNamespace.addChild("PID");
+    private static Supplier<Double> maxSpeed = shooterNamespace.addConstantDouble("Max Speed", 0.6);
+    private static Supplier<Double> minSpeed = shooterNamespace.addConstantDouble("Min Speed", 0);
+    public static Supplier<Double> shootSpeed = shooterNamespace.addConstantDouble("Speed", 0.4);
+
+    private static Supplier<Double> kP = PID.addConstantDouble("kP", 0);
+    private static Supplier<Double> kI = PID.addConstantDouble("kI", 0);
+    private static Supplier<Double> kD = PID.addConstantDouble("kD", 0);
+    private static Supplier<Double> tolerance = PID.addConstantDouble("Tolerance", 0);
+    private static Supplier<Double> waitTime = PID.addConstantDouble("Wait Time", 0);
+
+    private static Supplier<Double> targetSpeed = PID.addConstantDouble("Target Speed", 0);
+
+    public static PIDSettings velocityPIDSettings = new PIDSettings(kP, kI, kD, tolerance, waitTime);
+
+    private WPI_TalonSRX master;
+    private WPI_VictorSPX slave;
+    private NoiseReducer noiseReducer;
+
+    private ShootingSubsystem() {
+        super(minSpeed, maxSpeed);
+        master = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_MASTER);
+        slave = new WPI_VictorSPX(RobotMap.CAN.SHOOTER_SLAVE);
+//        master.setNeutralMode(NeutralMode.Brake);
+        TalonEncoder encoder = new TalonEncoder(master);
+//        master.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+        master.setInverted(true);
+//        slave.setNeutralMode(NeutralMode.Brake);
+        slave.follow(master);
+        noiseReducer = new NoiseReducer(() -> encoder.getVelocity() * distancePerPulse,
+                new ExponentialFilter(0.05));
+    }
+
+    @Override
+    public void apply(double speed) {
+        master.set(speed);
+    }
+
+    @Override
+    public boolean canMove(double speed) {
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        master.stopMotor();
+    }
+
+    @Override
+    public void periodic() {
+        shooterNamespace.update();
+    }
+
+    public double getMotorSpeed() {
+        return noiseReducer.get();
+    }
+
+    @Override
+    public void configureDashboard() {
+        shooterNamespace.putNumber("shooter velocity - filtered", noiseReducer);
+        shooterNamespace.putNumber("shooter velocity", () ->
+                (double) master.getSelectedSensorVelocity() * distancePerPulse);
+        shooterNamespace.putData("shoot", new MoveGenericSubsystem(this, shootSpeed));
+        shooterNamespace.putData("pid shoot",
+                new MoveGenericSubsystemWithPID(this, targetSpeed, this::getMotorSpeed, velocityPIDSettings));
+    }
+
+    public void setAccelerated(boolean isAccelerated) {
+        shooterNamespace.putBoolean("is accelerated", isAccelerated);
+    }
+}
