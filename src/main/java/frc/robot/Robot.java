0a46a6b342ba67594721a01bf9064ae4e29a12ea
OfriRosenbaum <ofriro1000>
2021-07-22 11:38:08 +0300

    a

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,9 +59,6 @@ public class Robot extends TimedRobot {
     public void disabledPeriodic() {
     }
 
-    /**
-     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-     */
     @Override
     public void autonomousInit() {
         // schedule the autonomous command (example)
